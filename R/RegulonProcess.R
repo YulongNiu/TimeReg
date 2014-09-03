69f2ce41849584febfa67cd48e31a5eb3207fa89
Yulong Niu <niuylscu@gmail.com>
2014-09-03 16:05:29 -0400

    add data documentation

--- R/RegulonProcess.R
+++ R/RegulonProcess.R
@@ -23,7 +23,7 @@ df2regulon <- function(dfFile) {
 } 
 
 
-##' Transfer data-frame regulor data to list.
+##' Transfer data-frame regulor data to list
 ##'
 ##' The regulon data file can be stored in a .txt or .csv file, which are read in and stored as data-frame. The row names are used as regulors' name, and the same regulors are arranged in into one column. There are three types of regulors, "activator", "repressor", and "notsure". The regulator data is sperated by "|", for example "repressor|SMU_1995c"
 ##' @title Data-frame to regulons list
@@ -31,7 +31,7 @@ df2regulon <- function(dfFile) {
 ##' @return A list of regulors
 ##' @examples
 ##' data(smuRegulors)
-##' smuRegulors <- df2regulon(smuRegulors)
+##' smuRegulors <- df2regulor(smuRegulors)
 ##' @author Yulong Niu \email{niuylscu@@gmail.com}
 ##' @export
 ##' 
@@ -51,3 +51,32 @@ df2regulor <- function(dfFile){
 }
 
 
+
+##' Streptococcus mutans regulons.
+##'
+##' A data-frame containing the S.mutans regulons, and the dataset is read from
+##' an csv file by read.csv(csvFilePath, header = TRUE). 
+##' Each column is the same regulons which maybe regulated by several regulators. 
+##' The first row is the regulons' name.
+##' @docType data
+##' @name smuRegulons
+##' @format A data frame
+##' @source \url{http://regprecise.lbl.gov/RegPrecise/genome.jsp?genome_id=37}
+##' @author Yulong Niu \email{niuylscu@@gmail.com}
+##' 
+NULL
+
+
+##' Streptococcus mutans regulors.
+##'
+##' A data-frame containing the S.mutans regulors, and the dataset is read from
+##' an csv file by read.csv(csvFilePath, header = TRUE). 
+##' Each column is the cluster of regulors which may regulate a cluster of regulons. 
+##' The first row is the name of regulator clusters.
+##' @docType data
+##' @name smuRegulors
+##' @format A data frame
+##' @source \url{http://regprecise.lbl.gov/RegPrecise/genome.jsp?genome_id=37}
+##' @author Yulong Niu \email{niuylscu@@gmail.com}
+##' 
+NULL
